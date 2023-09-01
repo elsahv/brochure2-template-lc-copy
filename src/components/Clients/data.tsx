0d59039bb4aa9f1ba5f11d7ed9dfb27732ca0cd6
Elsa <elsahvy@gmail.com>
2023-09-01 12:51:53 -0700

    site error fix

--- src/components/Clients/data.tsx
+++ src/components/Clients/data.tsx
@@ -1,7 +1,7 @@
 // import site1 from "../../images/site1.jpg";
 import site1 from "../../assets/screenshots/site1.png";
 import site2 from "../../assets/screenshots/site2.png";
-import site3 from "../../assets/screenshots/site3.png";
+// import site3 from "../../assets/screenshots/site3.png";
 // import site4 from "../../assets/screenshots/site4.png";
 // import site5 from "../../assets/screenshots/site5.png";
 // import site6 from "../../assets/screenshots/site6.png";
